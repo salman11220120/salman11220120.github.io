433d90c2c2bf3d01b94374bc4c34afc694958395
salman11220120 <150821333+salman11220120@users.noreply.github.com>
2023-11-14 21:44:33 +0700

    Add files via upload

---
+++ situs/cv.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>11220120 - Salman</title>
+</head>
+<body>
+<table border='1' width='100%' height='100%' cellspacing='0' cellpadding='0'>
+	<tr><td colspan='2' height='150' align='center'><img src='welcom.jpg' width='100%' height='150'></td></tr>
+	<tr><td valign='top'>
+	<table border='1' width='100%' cellspacing='0' cellpadding='0'>
+		<tr><th>Menu</a></th></tr>
+		<tr><td><a href='index.php'>Beranda</td></tr>
+		<tr><td><a href='cv.php'>CV</a></td></tr>
+		<tr><td><a href='galeri.php'>Galeri</a></td></tr>
+	</tr></table>
+	<td width='1700' valign='top'>
+	<img src='cv.png' width='100%' height='100%'>
+	</td>
+	<tr><td  colspan='2' height='40' align='center'>FOOTER</td></tr>
+
+</table>
+</body>
+</html>
