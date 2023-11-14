93bba568f8278a0d9c0135ca30140ce05540f4d9
salman11220120 <150821333+salman11220120@users.noreply.github.com>
2023-11-14 21:49:36 +0700

    Add files via upload

---
+++ galeri.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>11220120 - Salman</title>
+</head>
+<body>
+<table border='1' width='100%' height='100%' cellspacing='0' cellpadding='0'>
+	<tr><td colspan='2' height='150' align='center'><img src='welcom.jpg' width='100%' height='150'></td></tr>
+	<tr><td valign='top'>
+	<table border='1' width='100%' cellspacing='0' cellpadding='0'>
+		<tr><th>Menu</a></th></tr>
+		<tr><td><a href='index.php'>Beranda</td></tr>
+		<tr><td><a href='cv.php'>CV</a></td></tr>
+		<tr><td><a href='galeri.php'>Galeri</a></td></tr>
+	</tr></table>
+	<td width='1700' valign='top'><img src='gambar1.jpg' width='50%' height='75%'>
+	<img src='gambar2.jpg' width='50%' height='75%'>
+	<img src='gambar3.jpg' width='50%' height='75%'>
+	<img src='gambar4.jpg' width='50%' height='75%'>
+	</td>
+	<tr><td  colspan='2' height='40' align='center'>FOOTER</td></tr>
+
+
+</table>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+	<title>11220120 - Salman</title>
+</head>
+<body>
+<table border='1' width='100%' height='100%' cellspacing='0' cellpadding='0'>
+	<tr><td colspan='2' height='150' align='center'><img src='welcom.jpg' width='100%' height='300'></td></tr>
+	<tr><td valign='top'>
+	<table border='1' width='100%' cellspacing='0' cellpadding='0'>
+		<tr>
+			<th>Menu</a></th>
+		</tr>
+		<tr>
+			<td><a href='index.php'>Beranda</td>
+		</tr>
+		<tr>
+			<td><a href='cv.php'>CV</a></td>
+		</tr>
+		<tr>
+			<td><a href='galeri.php'>Galeri</a></td>
+		</tr>
+	</tr>
+	</table>
+	<td width='1700' valign='center' text-align='center'>SELAMAT DATANG</td>
+	<tr><td  colspan='2' height='40' align='center'><img src='thanks.jpg' width='100%' height='200'></td></tr>
+	
+
+
+</table>
+</body>
+</html>
